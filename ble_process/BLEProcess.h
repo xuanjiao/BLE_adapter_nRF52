599817bae4dcc682a3ce911c87704103bd05766b
xuanjiao <xuanjiao.zhu@gmail.com>
2020-01-12 20:36:44 +0100

    move ble functions from main.cpp to BLEProcess.h

---
+++ ble_process/BLEProcess.h
@@ -0,0 +1,170 @@
+#include "events/EventQueue.h"
+#include "platform/Callback.h"
+#include "platform/NonCopyable.h"
+ 
+#include "ble/BLE.h"
+#include "ble/Gap.h"
+#include "ble/GapAdvertisingParams.h"
+#include "ble/GapAdvertisingData.h"
+#include "ble/FunctionPointerWithContext.h"
+#include "ble/GattClient.h"
+
+#define DEVICE_NAME     "ACD52"
+
+
+
+
+
+#include "SEGGER_RTT.h"
+#define printf(...)  SEGGER_RTT_printf(0,__VA_ARGS__)
+
+class BLEProcess : private mbed::NonCopyable<BLEProcess>
+{
+private:
+    events::EventQueue &_event_queue;
+    BLE &_ble_interface;
+    mbed::Callback<void(BLE&, events::EventQueue&)> _post_init_cb;
+public:
+    BLEProcess(events::EventQueue &event_queue, BLE &ble_interface):
+        _event_queue(event_queue),
+        _ble_interface(ble_interface),
+        _post_init_cb(){
+    }
+    
+    ~BLEProcess()
+    {
+        stop();
+    }
+
+    // Subscription to the ble interface initialization event.
+    void on_init(mbed::Callback<void(BLE&, events::EventQueue&)> cb)
+    {
+        _post_init_cb = cb;
+    }
+ 
+
+    // Initialize the ble interface, configure it and start advertising.
+    bool start()
+    {
+        printf("Ble process start\n");
+
+        if(_ble_interface.hasInitialized()){
+            printf("Error: the ble instance has already been initialized.\n");
+            return false;
+        }
+
+        // It will be called every time the BLE stack has pending work. 
+        _ble_interface.onEventsToProcess(
+            makeFunctionPointer(this,&BLEProcess::schedule_ble_events)
+        );
+        
+        ble_error_t error = _ble_interface.init(this,&BLEProcess::when_init_complete);
+        if(error){
+            printf("Error: %u returned by BLE::init.\r\n", error);
+            return false;
+        }
+        return true;
+    }
+
+    // Close existing connection and stop process
+    void stop()
+    {
+        if(_ble_interface.hasInitialized()){
+            _ble_interface.shutdown();
+            printf("BLE process stopped");
+        }
+    }
+
+    private:
+
+    // Sets up adverting payload and start advertising.
+    void when_init_complete(BLE::InitializationCompleteCallbackContext *event)
+    {
+        if(event->error){
+            printf("Error %u during the initialization\r\n", event->error);
+            return;
+        }
+        printf("BLE instance initialized.\r\n");
+
+        Gap &gap = _ble_interface.gap();
+        ble_error_t error = gap.setAdvertisingPayload(make_advertising_data());
+        if(error){
+            printf("Error %u during gap.setAdvertisingPayload");
+        }
+
+        gap.setAdvertisingParams(make_advertising_params());
+
+        gap.onConnection(this, &BLEProcess::when_connection);
+        gap.onDisconnection(this, &BLEProcess::when_disconnection);
+
+        start_advertising();
+
+        if (_post_init_cb) {
+            _post_init_cb(_ble_interface, _event_queue);
+        }
+
+    }
+
+    // Start the gatt client process when a connection event is received.
+     void when_connection(const Gap::ConnectionCallbackParams_t *connection_event)
+     {
+         printf("Connected. \r\n");
+     }
+
+    // Stop the gatt client process when the device is disconnected then restart
+    // advertising.
+     void when_disconnection(const Gap::DisconnectionCallbackParams_t *event)
+     {
+         printf("Disconnected.\r\n");
+         start_advertising();
+     }
+
+     // Start the advertising process; it ends when a device connects.
+    void start_advertising()
+    {
+        ble_error_t error = _ble_interface.gap().startAdvertising();
+        if(error){
+            printf("Error %u during gap.startAdvertising.\r\n");
+        }else{
+            printf("Advertising start.\r\n");
+        }
+    }
+
+    // Schedule processing of events from the BLE middleware in the event queue.
+    void schedule_ble_events(BLE::OnEventsToProcessCallbackContext *event)
+    {
+        _event_queue.call(mbed::callback(&event->ble, &BLE::processEvents));
+    }
+
+    // Build data advertised by the BLE interface.
+    static GapAdvertisingData make_advertising_data(){
+        GapAdvertisingData advertising_data;
+
+        // add advertising flags
+        advertising_data.addFlags(
+            GapAdvertisingData::LE_GENERAL_DISCOVERABLE | // Peripheral device is discoverable at any moment.
+            GapAdvertisingData::BREDR_NOT_SUPPORTED     // Peripheral device is LE only.
+        );
+
+        // add device name
+        advertising_data.addData(
+            GapAdvertisingData::COMPLETE_LOCAL_NAME, 
+            (uint8_t *)DEVICE_NAME, 
+            strlen(DEVICE_NAME)
+        );
+
+        return advertising_data;
+    }
+
+    static GapAdvertisingParams make_advertising_params(){
+        return GapAdvertisingParams(
+            /* type */     GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED,
+            /* interval */ GapAdvertisingParams::MSEC_TO_ADVERTISEMENT_DURATION_UNITS(500),
+            /* timeout */  0
+        );
+    }
+
+    // Build advertising parameters used by the BLE interface.
+
+
+};
--- main.cpp
+++ main.cpp
@@ -1,202 +1,39 @@
-/* 
- * aconno.de
- * Made by Jurica Resetar
- * All right reserved
- *
- */
+#include<ble_process/BLEProcess.h>
 
-#include "mbed.h"
-#include "ble/BLE.h"
-#include "acd52832_bsp.h"
-#include "SEGGER_RTT.h"
-#include "GapAdvertisingData.h"
-#include "CurrentTimeService.h"
-#include "AckService.h"
-#include "nrf52_uart.h"
-#include "LDR/LDR.h"
 
-#define SLEEP_TIME      (5.0)           /* Sleep time in seconds */
-#define WAKE_UP_TIME    (100)          /* Awake time in ms */
-#define MSD_SIZE        (19)             /* Manufacturer Specific Data lenght (in B) */
-#define ADV_INTERVAL    (20)          /* Advertising interval in ms */
-#define TX_POWER        (4)             /* TX power (in dB) */
-#define DEVICE_NAME     "ACD"
+class GattProcess{
+    private:
+        BLE *_ble_interface;
+        events::EventQueue *_event_queue;
+   public:
+        void init(BLE &ble_interface, events::EventQueue &event_queue)
+        {
+            _ble_interface = &ble_interface;
+            _event_queue = &event_queue;
+          //  _client = &_ble_interface->gattClient();
 
-#define GO_TO_SLEEP     (0)             /* Sleep flag: 0 -> Device will not go to sleep, 1 -> Will go to sleep mode */
-#define PRINT           (0)
-
-#define printf(...)  SEGGER_RTT_printf(0,__VA_ARGS__)
-
-#if PRINT
-    #define TX          (p25)
-    #define RX          (p26)
-    NRF52_UART serial(TX, RX, Baud9600);
-    char buffer[sizeof(char)*255];
-    uint8_t dataWritten;
-#endif
-
-bool SLEEP = true;
-
-ACKService<4> *ackServicePtr;
-LDR ldr(ADC_LIGHT);
-static const uint16_t uuid16_list[] = {ACKService<4>::ACK_SERVICE_UUID};
-
-DigitalOut advLED(p22);         // Red
-DigitalOut scanLED(p23);        // Blue
-DigitalOut connectedLED(p24);   // Green
-    
-GapAdvertisingData adv_data = GapAdvertisingData();
-uint8_t MSD[MSD_SIZE] = {0x00, 0x59};
-uint8_t myMacAddress[6] = {};
-
-void onConnectionCallback(const Gap::ConnectionCallbackParams_t *params){   
-        scanLED = !scanLED;       // Blue
-        wait_ms(100);
-        scanLED = !scanLED;       // Blue
-        wait_ms(100);
-        scanLED = !scanLED;       // Blue
-        wait_ms(100);
-        scanLED = !scanLED;       // Blue
-        wait_ms(100);
-        scanLED = !scanLED;       // Blue
-        wait_ms(100);
-        scanLED = !scanLED;       // Blue
-        wait_ms(100);
-        scanLED = 1;              // Blue
-}
-
-void onDataWrittenCallback(const GattWriteCallbackParams *params) {
-    /*
-        This function should make advertiser stop
-    */
-    //dataWritten = sprintf(buffer, "Bok bok =) \n\r");
-    //serial.send(buffer, dataWritten);
-    
-    //ble.disconnect(Gap::LOCAL_HOST_TERMINATED_CONNECTION);
-    return;
-}
-
-/**
- * Restart Advertising on disconnection
- */
-void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params){
-    BLE::Instance().gap().startAdvertising();
-}
-
-/**
- *  Function for waking the core up
- */
-void wakeMeUp(void){
-        SLEEP = false;
-    }
-
-/**
- * This function is called when the ble initialization process has failed
- */
-void onBleInitError(BLE &ble, ble_error_t error){
-    /* Avoid compiler warnings */
-    (void) ble;
-    (void) error;
-    /* Initialization error handling should go here */
-}
-
-/**
- * Callback triggered when the ble initialization process has finished
- */
-void bleInitComplete(BLE::InitializationCompleteCallbackContext *params){
-    BLE&        ble   = params->ble;
-    ble_error_t error = params->error;
-
-    
-    
-    uint8_t init_values[4] = {0,0,0,0};
-    /* Get my MAC address */
-    BLEProtocol::AddressType_t temp_address_type;
-    ble.gap().getAddress(&temp_address_type, myMacAddress);
-    ackServicePtr = new ACKService<4>(ble, init_values);
-    ackServicePtr->updateMacAddress(myMacAddress);    // Update MAC address
-    
-    // add current date time service
-    BLE_DateTime initialDateTime = {
-        .year = 2020,
-        .month = 12,
-        .day = 01,
-        .hours = 20,
-        .minutes = 30,
-        .seconds = 50
-    };
-    CurrentTimeService currentTimeService(ble,initialDateTime);
-
-    ble.gap().onDisconnection(disconnectionCallback);
-    ble.gap().onConnection(onConnectionCallback);         // -->> Uncomment these two lines for shush-shush 
-    ble.gattServer().onDataWritten(onDataWrittenCallback);
-    
-    /* setup advertising */
-    //ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, (uint8_t *)uuid16_list, sizeof(uuid16_list));
-    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (uint8_t *)DEVICE_NAME, strlen(DEVICE_NAME));
-    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::MANUFACTURER_SPECIFIC_DATA, (uint8_t *)MSD, MSD_SIZE);
-    ble.gap().setAdvertisingInterval(500);  // --> Has to be at least 100ms!
-    ble.gap().startAdvertising();
-
-}
-
-void updateData(BLE *ble){    
-    static uint8_t simple_counter = 0;
-    
-    // Value normalized to a byte [0,255]
-    uint8_t uLight = ldr.getLight();
-    printf("light = %x\n",uLight);
-
-    MSD[2] = simple_counter++;
-    adv_data = ble->getAdvertisingData();
-    adv_data.updateData(adv_data.MANUFACTURER_SPECIFIC_DATA, &uLight, sizeof(uint8_t));
-    ble->setAdvertisingData(adv_data);
-}
-
-void RTCTimeSetup(){
-    set_time(1256729737);  // Set RTC time to Wed, 28 Oct 2009 11:35:37
-}
-
-void showCurrentTimeString(){
-    time_t seconds = time(NULL);
-
-    char buffer[40];
-    strftime(buffer, 40, "%a,%d %m %Y.%H:%M:%S\r", localtime(&seconds));
-    printf("Time as a custom formatted string = %s", buffer);
-}
-
-int main(void){
-    RTCTimeSetup();
-    showCurrentTimeString();
-
-    BLE &ble = BLE::Instance();
-    
-    advLED = 1;         // Red
-    scanLED = 1;        // Blue
-    connectedLED = 1;   // Green
-
-    Ticker ticker;
-    ticker.attach(wakeMeUp, SLEEP_TIME);   // Wake the device up   
-    
-    ble.init(bleInitComplete);
-    ble.gap().setTxPower(TX_POWER);        // Set TX power to TX_POWER
-    
-    /* SpinWait for initialization to complete. This is necessary because the
-     * BLE object is used in the main loop below. */
-    while (ble.hasInitialized()  == false) { /* spin loop */ }
-    while (true){
-        if (SLEEP && GO_TO_SLEEP){
-            ble.gap().stopAdvertising();
-            sleep();
-            ble.waitForEvent();
+          //  _ble_interface->gap().setEventHandler(this);
         }
-        else{
-            // I'm awake
-            updateData(&ble);
-            ble.gap().startAdvertising();
-            wait_ms(WAKE_UP_TIME);
-            ble.waitForEvent();
-            SLEEP = true;
-            }
-    }
-}
+
+};
+        int main(){
+            BLE &ble_interface = BLE::Instance();
+            events::EventQueue event_queue;
+            GattProcess gattProcess;
+            BLEProcess ble_process(event_queue,ble_interface);
+            
+            // Register GattClientProcess::init in the ble_process; this function will
+            // be called once the ble_interface is initialized.
+            ble_process.on_init(
+                mbed::callback(&gattProcess,&GattProcess::init)
+            );
+
+            // bind the event queue to the ble interface, initialize the interface
+            // and start advertising
+            ble_process.start();
+
+            // Process the event queue.
+            event_queue.dispatch_forever();
+
+            return 0;
+        }
